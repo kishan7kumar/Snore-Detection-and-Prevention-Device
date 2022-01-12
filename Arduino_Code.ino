6e7128afacb1964053b556b814a2a2efa2b2d267
Kishan Kumar <53033119+kishan7kumar@users.noreply.github.com>
2022-01-12 23:23:46 +0530

    Update Arduino_Code.ino

--- Arduino_Code.ino
+++ Arduino_Code.ino
@@ -1,6 +1,3 @@
-//********************PROGRAM FOR ANTI-SNORING ELECTRONIC DEVICE (ASED)****************************\\
-
-
 // defining Sound sensor
 int micpin = A0; // arduino analog pin A0 connected to sound sensor A0 pin
 int m1 = 0;
@@ -12,14 +9,13 @@ const int in4 = 3;
 // defining buzzer
 const int buzzer = 4;
 // defining leds
-int greenled = 5; //for showing air filling
+int greenled = 5;  //for showing air filling
 int blueled = 6;  // for showing snoring detetection
 int redled = 7;	  // for showing power on or off
 
 void setup()
 {
-	pinMode(micpin, INPUT);	 // gives input signals to the arduino board micvalue1 and micvalaue2
-							 // Serial.begin(9600);        // for displaying into serial monitor
+	pinMode(micpin, INPUT);	 // gives input signals to the arduino board micvalue1 and micvalaue2					
 	pinMode(buzzer, OUTPUT); // for buzzer
 	pinMode(in3, OUTPUT);	 // for L293D motor driver which is connected to Air Pump
 	pinMode(in4, OUTPUT);	 // for L293D motor driver which is connected to Air Pump
@@ -42,32 +38,29 @@ void setup()
 }
 
 void loop()
-
 {
 	digitalWrite(redled, HIGH);
-	Listen();										  // microphone starts to liten to snoring
-	if (m1 < 400 && m2 < 400 && m3 != m1 && m3 != m2) // when snoring starts then get inside loop
-	{												  //Serial.println("snoring starts------- filling air into pillow ");
-		FillAir();									  // air starts to fill inside air bag of pillow
-		delay(40000);								  // fill air for 35 seconds
+	Listen();// microphone starts to liten to snoring
+	if (m1 < 400 && m2 < 400 && m3 != m1 && m3 != m2) // snoring starts 
+	{	Serial.println("snoring starts------- filling air into pillow ");
+		FillAir(); // air starts to fill inside air bag of pillow
+		delay(40000)// fill air for 35 seconds
 		StopAir();
 		delay(2000);
-
 		for (int i = 1; i < 100; i++)
 		{
 			Listen();
 			if (m1 < 300 && m2 < 300)
-			{ //Serial.println("snoring too loud !!!!!!!!!! raising alarm ");
+			{       Serial.println("snoring too loud !!!!!!!!!! raising alarm ");
 				Alarmseq();
 				delay(2000);
 				break;
 			}
 		}
 	}
-} //loop function closed
+}
 
 // all functions defined here
-
 void Listen()
 {
 	digitalWrite(blueled, HIGH);
@@ -75,12 +68,13 @@ void Listen()
 	delay(90);
 	m2 = analogRead(micpin);
 }
+
 void PlayAlarm()
 {
 	digitalWrite(blueled, HIGH);
 	digitalWrite(greenled, HIGH);
 	tone(buzzer, 1000);
-	delay(500); // ...for 1 sec
+	delay(500); 
 }
 
 void StopAlarm()
@@ -111,19 +105,5 @@ void Alarmseq()
 	PlayAlarm();
 	StopAlarm();
 	PlayAlarm();
-	StopAlarm();
-	PlayAlarm();
-	StopAlarm();
-	PlayAlarm();
-	StopAlarm();
-	PlayAlarm();
-	StopAlarm();
-	PlayAlarm();
-	StopAlarm();
-	PlayAlarm();
-	StopAlarm();
-	PlayAlarm();
-	StopAlarm();
-	PlayAlarm();
-	StopAlarm();
+	StopAlarm();	
 }
